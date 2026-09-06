4c3627245d5812b565e69a45365aca2ed38c0f31
Hedgehogedgehog <130229563+Hedgehogedgehog@users.noreply.github.com>
2026-09-06 19:57:59 +0100

    Added log

--- root/installer/OSInstaller.lua
+++ root/installer/OSInstaller.lua
@@ -3,25 +3,49 @@ local repo = "CC-OS"
 local branch = "main"
 local baseURL = "https://raw.githubusercontent.com/" .. user .. "/" .. repo .. "/refs/heads/" .. branch .. "/"
 
-local sysFolders = {
-    --"root/installer/OSInstaller.lua",
-    "root/sys/startup.lua",
-}
+local rootPath = "root"
+
+local installerLogPath = "installerLog.txt"
+
+local function clearLog()
+    fs.delete(installerLogPath)
+end
+
+local function log(message)
+    print(message)
+    local file = fs.open(installerLogPath, "a")
+    file.writeLine(message)
+    file.close()
+end
 
 local function fetchFolder(folder, baseURL)
-    local link = baseURL .. folder
+    --local link = baseURL .. folder
 
-    print("Fetching folder: " , folder, " From: " , link)
+    --print("Fetching folder: " , folder, " From: " , link)
 
     --local request = http.get("https://raw.githubusercontent.com/Hedgehogedgehog/CC-OS/refs/heads/main/root/sys/startup.lua")
     --local request = http.get("https://example.tweaked.cc")
-    local request = http.get(link)
-    print(request.readAll())
+    --local request = http.get(link)
+    --print(request.readAll())
 
+    local folderPath = folder
+
+    local APIURL = ("https://api.github.com/repos/%s/%s/contents/%s?ref=%s"):format(user, repo, folder, branch)
+
+    local APIResponse = http.get(APIURL)
+
+    if not APIResponse then 
+        log("Failed to fetch folder if you have a older version of CC-OS installed it will continue to work else try to install agien later")
+        return
+    end
+    
+    ---@type table
+    local contents = textutils.unserialiseJSON(APIResponse.readAll())
+    log(textutils.serialize(contents))
 end
 
 local function configComputerSettings()
-    print("Configuring Computer Settings")
+    log("Configuring Computer Settings")
 
     shell.run("Set motd.enable false")
 
@@ -30,18 +54,17 @@ end
 
 
 local function installOS()
-    print("Installing OS")
-    for _,folder in pairs(sysFolders) do
-        fetchFolder(folder, baseURL)
-    end
+    log("Installing OS")
+    fetchFolder("root", baseURL)
 
     --shell.run("clear")
 end
 
 local function readyInstall()
+    clearLog()
     configComputerSettings()
     installOS()
 end
 
 readyInstall()
-print("install complete")
+log("install complete")
