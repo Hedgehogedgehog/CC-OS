1febf200165e871efd834d2cb1c1e709cdc30dec
Hedgehogedgehog <130229563+Hedgehogedgehog@users.noreply.github.com>
2026-09-06 19:24:16 +0100

    setup fetching files

--- root/installer/OSInstaller.lua
+++ root/installer/OSInstaller.lua
@@ -1,11 +1,11 @@
-local user = "Hedghogedgehog"
+local user = "Hedgehogedgehog"
 local repo = "CC-OS"
 local branch = "main"
 local baseURL = "https://raw.githubusercontent.com/" .. user .. "/" .. repo .. "/refs/heads/" .. branch .. "/"
 
 local sysFolders = {
-    "root/installer",
-    "root/sys"
+    --"root/installer/OSInstaller.lua",
+    "root/sys/startup.lua",
 }
 
 local function fetchFolder(folder, baseURL)
@@ -13,20 +13,35 @@ local function fetchFolder(folder, baseURL)
 
     print("Fetching folder: " , folder, " From: " , link)
 
+    --local request = http.get("https://raw.githubusercontent.com/Hedgehogedgehog/CC-OS/refs/heads/main/root/sys/startup.lua")
     --local request = http.get("https://example.tweaked.cc")
     local request = http.get(link)
     print(request.readAll())
 
 end
 
+local function configComputerSettings()
+    print("Configuring Computer Settings")
+
+    shell.run("Set motd.enable false")
+
+    shell.run("clear")
+end
+
+
 local function installOS()
     print("Installing OS")
     for _,folder in pairs(sysFolders) do
         fetchFolder(folder, baseURL)
     end
+
+    --shell.run("clear")
 end
 
+local function readyInstall()
+    configComputerSettings()
+    installOS()
+end
 
-
-
-installOS()
+readyInstall()
+print("install complete")
