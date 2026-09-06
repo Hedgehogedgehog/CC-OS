e3371fdecaad019f2a7268884b20971af4f86bc6
Hedgehogedgehog <130229563+Hedgehogedgehog@users.noreply.github.com>
2026-09-06 19:00:10 +0100

    Started installer

---
+++ root/installer/OSInstaller.lua
@@ -0,0 +1,32 @@
+local user = "Hedghogedgehog"
+local repo = "CC-OS"
+local branch = "main"
+local baseURL = "https://raw.githubusercontent.com/" .. user .. "/" .. repo .. "/refs/heads/main/" .. branch .. "/"
+
+local sysFolders = {
+    "root/installer",
+    "root/sys"
+}
+
+local function fetchFolder(folder, baseURL)
+    local link = baseURL .. folder
+
+    print("Fetching folder: " , folder, " From: " , link)
+
+    --local request = http.get("https://example.tweaked.cc")
+    local request = http.get(link)
+    print(request.readAll())
+
+end
+
+local function installOS()
+    print("Installing OS")
+    for _,folder in pairs(sysFolders) do
+        fetchFolder(folder, baseURL)
+    end
+end
+
+
+
+
+installOS()
